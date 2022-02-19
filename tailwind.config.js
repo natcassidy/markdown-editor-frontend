1cca2a46f24f67f8e7b5adbcc3fb31cef5025a93
natcassidy <nat.cassidy865@gmail.com>
2022-02-19 18:50:38 -0400

    Fix for netlify issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,13 @@ module.exports = {
   },
   plugins: [],
 }
+
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
