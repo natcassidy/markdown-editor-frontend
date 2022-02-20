7a17acd5acbd0700ced83e44afb273dc4a21a1c1
natcassidy <nat.cassidy865@gmail.com>
2022-02-19 21:36:05 -0400

    Fixed useState import

--- src/Components/Authentication.js
+++ src/Components/Authentication.js
@@ -1,5 +1,4 @@
-import React, { useContext } from 'react'
-import { useState } from 'react/cjs/react.development'
+import React, { useContext, useState } from 'react'
 import { UserContext } from '../UserContext'
 import CreateUser from './CreateUser'
 
